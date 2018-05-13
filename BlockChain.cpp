e5fdd09da5c70468914e1c584edd3070c6522ded
pankajnitp <pankajkr.nitp@gmail.com>
2018-05-13 14:38:45 +0530

    Update BlockChain.cpp

--- BlockChain.cpp
+++ BlockChain.cpp
@@ -5,11 +5,24 @@ using namespace std;
 #include <stdio.h>
 
 struct _node {
-	int		_child[20];
+	int	_child[20];
 	short	_amount[16];
-};
+} nodePool[15000];
 
-_node nodePool[15000];
+struct _trieEntry{
+	_trieEntry* child[16];
+	int		_id;
+} *trieHead = NULL;
+
+void insertTrie(char* hash)
+{
+	_trieEntry* ptr = trieHead;
+	
+}
+void init()
+{
+	trieHead = (_trieEntry*) calloc(1, sizeof(_trieEntry));
+}
 
 int main()
 {
