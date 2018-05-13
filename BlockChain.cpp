480257e993f2a2486c1f3fc7337d7444cb7cc7e3
pankajnitp <pankajkr.nitp@gmail.com>
2018-05-13 14:11:47 +0530

    Create BlockChain.cpp
    Very first idea

---
+++ BlockChain.cpp
@@ -0,0 +1,17 @@
+#pragma GCC optimize ("-Ofast")
+#include <iostream>
+using namespace std;
+
+#include <stdio.h>
+
+struct _node {
+	int		_child[20];
+	short	_amount[16];
+};
+
+_node nodePool[15000];
+
+int main()
+{
+	return 0;
+}
