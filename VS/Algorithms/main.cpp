10efd731aa17a9b3d07894984f1b42a60b6cdab1
Pankaj Kumar <pankajkr.nitp@gmail.com>
2020-02-08 22:09:44 +0530

    Folder structure changed Implemented vector in C++ using std::move

--- VS/Algorithms/main.cpp
+++ VS/Algorithms/main.cpp
@@ -1,25 +1,13 @@
 #include<iostream>
-#include "shared_ptr.h"
-#include "math_util.h""
+#include "testcases.h"
 
 using namespace std;
 
 int main()
 {
-	Element<int> el1(3);
-	Element<int> el2(5);
-
-	outer(17, 29);
-	cout << el2 + el1 << endl;
-
-	Shared_ptr<int> ptr1(new int(151));
-	cout << "--- Shared pointers ptr1, ptr2 ---\n";
-	{
-		Shared_ptr<int> ptr2 = ptr1;
-		cout << ptr1;
-		cout << ptr2;
-	}
-	cout << ptr1;
+	//math_tc_main();
+	//shared_ptr_tc_main();
+	vector_tc_main();
 
 	return 0;
 }
--- VS/Algorithms/math_util.h
+++ VS/Algorithms/math_util.h
@@ -17,18 +17,3 @@ private:
 	T internal;
 };
 
-void inner(int &a, int &b)
-{
-	cout << "#1" << endl;
-}
-void inner(int &&a, int &&b)
-{
-	cout << "#2" << endl ;
-}
-
-template <typename T1, typename T2> 
-void outer(T1&& t1, T2&& t2)
-{
-	inner(std::forward<T1>(t1), std::forward<T2>(t2));
-	//inner(t1, t2);
-}
